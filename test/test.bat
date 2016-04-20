3897f0b7d6f711e21cf1638437bc8154ffefeb66
Yuki Suga <ysuga@ysuga.net>
2016-04-20 16:00:09 +0900

    update for package for pypi

--- test/test.bat
+++ test/test.bat
@@ -1,6 +1,7 @@
 rem rmdir /S /Q BasicDataType ExtendedDataTypes InterfaceDataTypes
-python project_generator.py BasicDataType.idl
+rem python project_generator.py BasicDataType.idl
 rem python project_generator.py ExtendedDataTypes.idl -I.
 rem python project_generator.py InterfaceDataTypes.idl -I.
-python project_generator.py CarSimulator.idl -I.
+rem python project_generator.py CarSimulator.idl -I.
 
+python ../generate_adaptor.py BasicDataType.idl -bc
